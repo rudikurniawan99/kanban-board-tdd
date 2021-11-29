81510d4ed09a3e493a63b2f3a5a22c20424bcf3e
rudikurniawan99 <rudikurniawan501@gmail.com>
2021-11-30 00:15:22 +0700

    test making one test file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div className="container mx-auto">
-      <h1 className="text-red-500 text-3xl">Ini coba h1 yaa</h1> 
+      <h1 className="text-red-500 text-3xl">Kanban Board</h1> 
+      <button className="py-1 px-3 bg-blue-500 rounded-md text-white mt-4 hover:bg-blue-400">Add List</button>
     </div>
   )
 }
